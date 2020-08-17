bb00d8f4e27adb0996a68883d98ced058b5f1252
zhenhua.yu <zhenhua.yu@utranshub.com>
2020-08-17 13:34:02 +0800

    1

---
+++ DesignPatterns/DesignPatterns/eventMode.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns
+{
+    public class Cat
+    {
+        public event Action CatCome;   //声明一个事件
+
+        private string name;
+
+        public Cat(string name)
+        {
+            this.name = name;
+        }
+        public void CatComing()
+        {
+            Console.WriteLine("猫" + name + "来了");
+            CatCome?.Invoke();
+        }
+    }
+
+    public class Mouse
+    {
+        private string name;
+
+        public Mouse(string name, Cat cat)
+        {
+            this.name = name;
+            cat.CatCome += this.RunAway;        //Mouse 注册 CatCome 主题
+        }
+        public void RunAway()
+        {
+            Console.WriteLine(name + "正在逃跑");
+        }
+    }
+}
