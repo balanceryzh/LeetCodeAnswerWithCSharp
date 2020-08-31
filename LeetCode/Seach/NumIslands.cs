7f25370124450db68e795abe1257fd4be4189b8f
zhenhua.yu <zhenhua.yu@utranshub.com>
2020-08-31 17:09:01 +0800

    1

--- LeetCode/Seach/NumIslands.cs
+++ LeetCode/Seach/NumIslands.cs
@@ -1,9 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Versioning;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace ConsoleTest.Seach
 {
+
+    //给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+
+//岛屿总是被水包围，并且每座岛屿只能由水平方向或竖直方向上相邻的陆地连接形成。
+
+//此外，你可以假设该网格的四条边均被水包围。
+
+ 
+
+//示例 1:
+
+//输入:
+//[
+//['1','1','1','1','0'],
+//['1','1','0','1','0'],
+//['1','1','0','0','0'],
+//['0','0','0','0','0']
+//]
+//输出: 1
+//示例 2:
+
+//输入:
+//[
+//['1','1','0','0','0'],
+//['1','1','0','0','0'],
+//['0','0','1','0','0'],
+//['0','0','0','1','1']
+//]
+//输出: 3
+//解释: 每座岛屿只能由水平和/或竖直方向上相邻的陆地连接而成。
+
     public class NumIslands
     {
         public int NumIslands2(char[][] grid)
@@ -37,6 +70,51 @@ namespace ConsoleTest.Seach
                 DFS(x, y - 1, grid);
         }
 
+        public int NumIslands3(char[][] grid)
+        {
+            if(grid.Length==0)
+            {
+                return 0;
+            }
+            int landnum = 0;
+            for(int i=0;i<grid.Length;i++)
+            {
+                for(int j=0;j<grid[0].Length;j++)
+                {
+                    if(grid[i][j]!='0')
+                    {
+                        DFS3(i, j, grid);
+                        landnum++;
+                    }
 
+                }
+            }
+
+
+            return landnum;
+
+        }
+
+        public void DFS3(int x, int y, char[][] grid)
+        {
+            grid[x][y] = '0';
+
+            if(x+1<grid.Length&&grid[x+1][y]=='1')
+            {
+                DFS3(x + 1, y, grid);
+            }
+            if (x - 1 >=0 && grid[x - 1][y] == '1')
+            {
+                DFS3(x - 1, y, grid);
+            }
+            if (y+ 1 < grid[0].Length && grid[x][y+1] == '1')
+            {
+                DFS3(x, y+1, grid);
+            }
+            if (y - 1 >= 0 && grid[x][y-1] == '1')
+            {
+                DFS3(x, y-1, grid);
+            }
+        }
     }
 }
---
+++ LeetCode/StackQeueue/MinRemoveToMakeValid.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleTest.StackQeueue
+{
+    public class MinRemoveToMakeValid
+    {
+
+//        给你一个由 '('、')' 和小写字母组成的字符串 s。
+
+//你需要从字符串中删除最少数目的 '(' 或者 ')' （可以删除任意位置的括号)，使得剩下的「括号字符串」有效。
+
+//请返回任意一个合法字符串。
+
+//有效「括号字符串」应当符合以下 任意一条 要求：
+
+//空字符串或只包含小写字母的字符串
+//可以被写作 AB（A 连接 B）的字符串，其中 A 和 B 都是有效「括号字符串」
+//可以被写作(A) 的字符串，其中 A 是一个有效的「括号字符串」
+ 
+
+//示例 1：
+
+//输入：s = "lee(t(c)o)de)"
+//输出："lee(t(c)o)de"
+//解释："lee(t(co)de)" , "lee(t(c)ode)" 也是一个可行答案。
+//示例 2：
+
+//输入：s = "a)b(c)d"
+//输出："ab(c)d"
+//示例 3：
+
+//输入：s = "))(("
+//输出：""
+//解释：空字符串也是有效的
+//示例 4：
+
+//输入：s = "(a(b(c)d)"
+//输出："a(b(c)d)"
+ 
+
+//提示：
+
+//1 <= s.length <= 10^5
+//s[i] 可能是 '('、')' 或英文小写字母
+
+
+
+        public String minRemoveToMakeValid(String s)
+        {
+
+            // Parse 1: Remove all invalid ")"
+            StringBuilder sb = new StringBuilder();
+            int openSeen = 0;
+            int balance = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+                if (c == '(')
+                {
+                    openSeen++;
+                    balance++;
+                }
+                if (c == ')')
+                {
+                    if (balance == 0) continue;
+                    balance--;
+                }
+                sb.Append(c);
+            }
+
+            // Parse 2: Remove the rightmost "("
+            StringBuilder result = new StringBuilder();
+            int openToKeep = openSeen - balance;
+            for (int i = 0; i < sb.Length; i++)
+            {
+                char c = sb[i];
+                if (c == '(')
+                {
+                    openToKeep--;
+                    if (openToKeep < 0) continue;
+                }
+                result.Append(c);
+            }
+
+            return result.ToString();
+        }
+
+        public string MinRemoveToMakeValid2(string s)
+        {
+            StringBuilder sb = new StringBuilder();
+            int openCount = 0;
+            int balanceCount = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(')
+                {
+                    openCount++;
+                    balanceCount++;
+                }
+                else if (s[i] == ')')
+                {
+                    if (balanceCount == 0) continue;
+                    balanceCount--;
+                }
+                sb.Append(s[i]);
+            }
+            string str = sb.ToString();
+            sb = new StringBuilder();
+            int BalancedOpenCount = openCount - balanceCount;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == '(')
+                {
+                    if (BalancedOpenCount <= 0) continue;
+                    BalancedOpenCount--;
+                }
+                sb.Append(str[i]);
+            }
+            return sb.ToString();
+        }
+
+        public string MinRemoveToMakeValid3(string s)
+        {
+            StringBuilder s1 = new StringBuilder();
+            int open = 0;
+            int close = 0;
+            for(int i=0;i<s.Length;i++)
+            {
+                if(s[i]=='(')
+                {
+                    open++;
+                    close++;
+                }
+                if(s[i]==')')
+                {
+                    if (close <= 0) continue;
+                    close--;
+                }
+                s1.Append(s[i]);
+            }
+            StringBuilder s2 = new StringBuilder();
+            int outs = open - close;
+            for(int i=0;i<s1.Length;i++)
+            {
+                if(s1[i]=='(')
+                {
+                    if (outs <= 0) continue;
+                    outs--;
+                }
+                s2.Append(s1[i]);
+            }
+            return s2.ToString();
+        }
+    }
+}
