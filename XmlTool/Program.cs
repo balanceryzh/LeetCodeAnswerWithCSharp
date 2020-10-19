95abf0c56e75a5d9b15c3a38bd42360afc8cf2bb
zhenhua.yu <zhenhua.yu@utranshub.com>
2020-10-19 13:39:02 +0800

    1

---
+++ XmlTool/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace XmlTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+     
+    }
+}
